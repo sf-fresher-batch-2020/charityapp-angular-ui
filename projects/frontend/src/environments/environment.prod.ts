4e05968671ad1aecbd4e3465b10623387b704a6f
PriraRoja <72965298+PriraRoja@users.noreply.github.com>
2020-12-03 18:10:19 +0530

    Update environment.prod.ts

--- projects/frontend/src/environments/environment.prod.ts
+++ projects/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL : "http://localhost:5000/api"
-  //API_URL : "http:/your project-api.herokuapp.com/api"
+  //API_URL : "http://localhost:5000/api"
+  API_URL : "https://dashboard.heroku.com/apps/charity-rest-api"
 };
